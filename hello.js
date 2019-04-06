441b6c7c031a3a77e693f94bf51a52d3a42ac845
archit23081997 <49343712+archit23081997@users.noreply.github.com>
2019-04-06 20:09:27 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env nodejs
+var http = require('http');
+
+http.createServer(function (req, res){
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.write('Hello World!');
+	res.end();
+}).listen(8081, 'localhost');
+console.log('running');
